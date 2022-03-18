99795cad0bd0f66458cad8c0cc8d7508c1a64255
a-klimovich <ar2r.klimovich@gmail.com>
2022-03-18 13:11:50 +0100

    add: mob-menu / fiixes

--- src/js/main.js
+++ src/js/main.js
@@ -15,5 +15,16 @@
 
   HamburgerBtn.addEventListener('click', () => {
     HamburgerBtn.classList.toggle('active')
+    document.querySelector(".nav__menu-mobile").classList.toggle('active')
+  })
+
+  let isVideoPLay = false;
+
+  document.querySelector("body > div > main > div > sectin > div > div.video-payer").addEventListener('click', () => {
+    isVideoPLay = !isVideoPLay
+    console.log(isVideoPLay);
+
+    isVideoPLay ? document.querySelector("#videoPlayer").play() : document.querySelector("#videoPlayer").pause()
+    document.querySelector("body > div > main > div > sectin > div > div.video-payer > button").classList.toggle('hidden')
   })
 })()
