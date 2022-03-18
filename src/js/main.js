40d7357fd951baf51d3870aebdafff5119b6c71d
a-klimovich <ar2r.klimovich@gmail.com>
2022-03-18 13:38:40 +0100

    fix path

--- src/js/main.js
+++ src/js/main.js
@@ -15,16 +15,22 @@
 
   HamburgerBtn.addEventListener('click', () => {
     HamburgerBtn.classList.toggle('active')
-    document.querySelector(".nav__menu-mobile").classList.toggle('active')
+    
+    if (document.querySelector(".nav__menu-mobile")) {
+      document.querySelector(".nav__menu-mobile").classList.toggle('active')
+    }
   })
 
   let isVideoPLay = false;
 
-  document.querySelector("body > div > main > div > sectin > div > div.video-payer").addEventListener('click', () => {
-    isVideoPLay = !isVideoPLay
-    console.log(isVideoPLay);
-
-    isVideoPLay ? document.querySelector("#videoPlayer").play() : document.querySelector("#videoPlayer").pause()
-    document.querySelector("body > div > main > div > sectin > div > div.video-payer > button").classList.toggle('hidden')
-  })
+  if (document.querySelector('.video-payer')) {
+    document.querySelector('.video-payer').addEventListener('click', () => {
+      isVideoPLay = !isVideoPLay
+      console.log(isVideoPLay);
+  
+      isVideoPLay ? document.querySelector("#videoPlayer").play() : document.querySelector("#videoPlayer").pause()
+      document.querySelector("body > div > main > div > sectin > div > div.video-payer > button").classList.toggle('hidden')
+    })
+  }
+  
 })()
